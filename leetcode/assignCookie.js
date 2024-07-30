490c6ca65704ab5f35faa38d3901b719a4a4990d
Ahamed Kabeer <ak.tech.27.coder@gmail.com>
2024-07-30 21:39:53 +0530

    Create assignCookie.js

---
+++ leetcode/assignCookie.js
@@ -0,0 +1,36 @@
+// Leetcode 455
+
+/*
+Assume you are an awesome parent and want to give your children some cookies. But, you should give each child at most one cookie.
+
+Each child i has a greed factor g[i], which is the minimum size of a cookie that the child will be content with; and each cookie j has a size s[j]. If s[j] >= g[i], we can assign the cookie j to the child i, and the child i will be content. Your goal is to maximize the number of your content children and output the maximum number.
+*/
+
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function (g, s) {
+  if (!g.length || !s.length) {
+    return 0;
+  }
+  g.sort((a, b) => a - b);
+  s.sort((a, b) => a - b);
+  let count = 0;
+  let i = 0;
+  let j = 0;
+  while (j < s.length) {
+    if (g[i] <= s[j]) {
+      count++;
+      i++;
+    }
+    j++;
+  }
+  return count;
+};
+
+console.log(findContentChildren([1, 2, 3], [1, 1]));
+console.log(findContentChildren([1, 2], [1, 2, 3]));
+console.log(findContentChildren([1, 2, 3], []));
+console.log(findContentChildren([10, 9, 8, 7], [5, 6, 7, 8]));
