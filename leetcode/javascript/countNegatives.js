237f531905b6f721c39a8861ae922d6404789be1
Ahamed Kabeer <ak.tech.27.coder@gmail.com>
2025-12-28 07:10:42 +0530

    js: Leetcode 1351

---
+++ leetcode/javascript/countNegatives.js
@@ -0,0 +1,33 @@
+// Leetcode 1351
+
+/*
+Given a m x n matrix grid which is sorted in non-increasing order both row-wise and column-wise, return the number of negative numbers in grid.
+
+Example 1:
+Input: grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+Output: 8
+Explanation: There are 8 negatives number in the matrix.
+
+Example 2:
+Input: grid = [[3,2],[1,0]]
+Output: 0
+*/
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var countNegatives = function (grid) {
+  let count = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] < 0) {
+        count++;
+      }
+    }
+  }
+  return count;
+};
+
+console.log(countNegatives([[4, 3, 2, -1], [3, 2, 1, -1], [1, 1, -1, -2], [-1, -1, -2, -3]]));
+console.log(countNegatives([[3, 2], [1, 0]]));
